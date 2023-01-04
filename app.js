b273f50b992c2ff8b7850a8ac7e1add4d8632aa0
Krishnendu Mondal <mr.kri8eve@gmail.com>
2023-01-04 17:13:29 +0530

    dynamic data added

--- app.js
+++ app.js
@@ -13,8 +13,54 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
- 
-  return res.render("index", { title: "Panorama", layout: "./layouts/main" });
+  const bannerImages = [
+    "images/valeriia-bugaiova-_pPHgeHz1uk-unsplash.jpg",
+    "https://images.unsplash.com/photo-1606402179428-a57976d71fa4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "https://images.unsplash.com/photo-1584132967334-10e028bd69f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  ];
+  const restuarantImages = [
+    "emy-XoByiBymX20-unsplash.jpg",
+    "charlesdeluvio-D-vDQMTfAAU-unsplash.jpg",
+    "brooke-lark-M4E7X3z80PQ-unsplash.jpg",
+    "alex-munsell-Yr4n8O_3UPc-unsplash.jpg",
+  ];
+  const waterparkImages = [
+    "yianni-mathioudakis-JK1TD9Sp5kY-unsplash.jpg",
+    "jeremiah-lawrence-QBN_X4H0x9U-unsplash.jpg",
+    "aquavera-KzUAW2htbiI-unsplash.jpg",
+    "vince-fleming--gwECgLmmU8-unsplash.jpg",
+  ];
+  const services = [
+    {
+      imageUrl :'images/multicuisineRestuarant.jpg',
+      title:'Multi-Cuisine Restuarant'
+    },
+    {
+      imageUrl :'images/amusementPark.png',
+      title:'Amusement Park'
+    },
+    {
+      imageUrl :'images/gymFitness.jpg',
+      title:'Gym & Fitness'
+    },
+    {
+      imageUrl :'images/banquet.jpg',
+      title:'Banquet Hall'
+    },
+    {
+      imageUrl :'images/wedding-mandap-taiyaari-shuru.jpg',
+      title:'Events (Marriage/Corporate)'
+    },
+  ]
+  
+  return res.render("index", {
+    title: "Panorama",
+    layout: "./layouts/main",
+    waterparkImages,
+    restuarantImages,
+    bannerImages,
+    services
+  });
 });
 app.get("/contact", (req, res) => {
   return res.render("contact", {
